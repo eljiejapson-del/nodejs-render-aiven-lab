30bd04c9e0e1b69c0b8fe0d25aa1b2d1974468ef
Eljie Peteros <eljiejapson@gmail.com>
2026-04-14 12:02:31 +0800

    commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,s
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT
 });
